fe5bb6541ba4bf991568d333923743c92e957537
deepti <deepti@newput.com>
2015-10-28 11:15:15 +0530

    Responsive app ip

--- src/main/java/com/newput/utility/EMailSender.java
+++ src/main/java/com/newput/utility/EMailSender.java
@@ -6,6 +6,8 @@ import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.MailParseException;
 import org.springframework.mail.SimpleMailMessage;
@@ -23,6 +25,7 @@ import com.newput.domain.Employee;
  *         is @Autowired with bean defined in the applicationContext.xml
  */
 @Service
+//@PropertySource("classpath:Tracker.properties")
 public class EMailSender {
 
 	@Autowired
@@ -34,30 +37,42 @@ public class EMailSender {
 	@Autowired
 	private JsonResService jsonResService;
 
-	/**
-	 * Description : Use to send the verification mail to the user for registration and password reset.
-	 * @param module - password or registration
-	 */
-	public void sendMail(String module) {
+//	@Value("${Tracker.ip}")
+//	private String defaultIp;
 
+	/**
+	 * Description : Use to send the verification mail to the user for
+	 * registration and password reset.
+	 * 
+	 * @param module
+	 *            - password or registration
+	 */
+	public String sendMail(String module) {
+   try{
 		SimpleMailMessage email = new SimpleMailMessage();
 		email.setTo(emp.getEmail());
 		email.setSubject("Confirmation Mail");
 		if (module.equalsIgnoreCase("registration")) {
-			email.setText("Welcome, You are successfully register Please click here http://tracker/login?ET="
-					+ emp.getvToken());
+			email.setText("Welcome, You are successfully register Please click here http://" + System.getenv("App_Ip")
+					+ "/tracker/login?ET=" + emp.getvToken());
 		} else if (module.equalsIgnoreCase("password")) {
 			email.setText("Welcome, Please confirm your mail id. click here http://tracker/login?PT=" + emp.getpToken()
 					+ "&id=" + emp.getId());
 		}
 		mailSender.send(email);
+		return null;
+   }catch(Exception ex){
+	   return "Email id is not valid to send email.";
+   }
 	}
 
-	
 	/**
 	 * Description : Use to send the time sheet on the registered mail id.
-	 * @param email - abc.xyz@newput.com
-	 * @param file - excelFile
+	 * 
+	 * @param email
+	 *            - abc.xyz@newput.com
+	 * @param file
+	 *            - excelFile
 	 */
 	public void sendExcelSheet(String email, File file) {
 		MimeMessage message = mailSender.createMimeMessage();
@@ -80,8 +95,11 @@ public class EMailSender {
 	}
 
 	/**
-	 * Description : Use to send the reminder notification to user to fill the time sheet.
-	 * @param emp - An Object
+	 * Description : Use to send the reminder notification to user to fill the
+	 * time sheet.
+	 * 
+	 * @param emp
+	 *            - An Object
 	 */
 	public void notificationMail(Employee emp) {
 
