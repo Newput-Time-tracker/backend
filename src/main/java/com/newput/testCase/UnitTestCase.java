303f9b4f5ebf62af6964e3835b0a1bbd4cbf003d
deepti <deepti@newput.com>
2015-10-29 16:59:33 +0530

    Add Unit test cases on modules

--- src/main/java/com/newput/testCase/UnitTestCase.java
+++ src/main/java/com/newput/testCase/UnitTestCase.java
@@ -7,7 +7,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+
+import com.newput.domain.DateSheet;
 import com.newput.domain.Employee;
+import com.newput.domain.EmployeeExample;
+import com.newput.domain.Session;
+import com.newput.domain.SessionExample;
+import com.newput.domain.TimeSheet;
+import com.newput.mapper.DateSheetMapper;
+import com.newput.mapper.EmployeeMapper;
+import com.newput.mapper.SessionMapper;
 import com.newput.service.EmpService;
 import com.newput.service.LoginService;
 import com.newput.service.SelectiveExcel;
@@ -19,21 +28,33 @@ import com.newput.utility.ReqParseService;
 import com.newput.utility.TTUtil;
 
 import static org.junit.Assert.*;
-
 import java.io.File;
 import java.math.BigDecimal;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "/applicationContext.xml")
 public class UnitTestCase {
 
 	@Autowired
+	private DateSheet dateSheet;
+
+	@Autowired
+	DateSheetMapper dateSheetMapper;
+
+	@Autowired
+	private TimeSheet timeSheet;
+
+	@Autowired
+	private Session session;
+
+	@Autowired
 	private Employee emp;
-	
+	//
 	@Autowired
 	private EmpService empService;
 	
@@ -45,7 +66,7 @@ public class UnitTestCase {
 
 	@Autowired
 	private JsonResService jsonResService;
-	
+	//
 	@Autowired
 	private LoginService loginService;
 	
@@ -60,6 +81,12 @@ public class UnitTestCase {
 	
 	@Autowired
 	private SelectiveExcel excel;
+	
+	@Autowired
+	EmployeeMapper empMapper;
+
+	@Autowired
+	private SessionMapper sessionMapper;
 	
 	
 	public Long getCurrentTime() {
@@ -183,6 +210,137 @@ public class UnitTestCase {
 		excel.monthSheet(month, Integer.parseInt(empId), year);
 		assertEquals(true, jsonResService.isSuccess());
 		assertNotNull(jsonResService.getData());		
+	}
+	
+	
+	/*
+	 * Test Cases for SERVICE CLASS
+	 */
+	
+	
+	@Test
+	@Ignore
+	public void testAddUser() throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+		Date userDob = (Date) sdf.parse("10-10-1990");
+		Date userDoj = (Date) sdf.parse("10-10-2015");
+		emp.setAddress("Indore");
+		emp.setContact("0123456789");
+		emp.setCreated(1446091979L);
+		emp.setDob(userDob);
+		emp.setDoj(userDoj);
+		emp.setEmail("abc@newput.com");
+		emp.setFirstName("abc");
+		emp.setGender("f");
+		emp.setLastName("xyz");
+		emp.setPassword("vuhduhuhd");
+		emp.setvToken("8569");
+		int i = empMapper.insertSelective(emp);
+		assertEquals(true, i > 0);
+	}
+
+	@Test
+	@Ignore
+	public void testMailVerify() {
+		emp.setEmail("abc@newput.com");
+		EmployeeExample example = new EmployeeExample();
+		example.createCriteria().andEmailEqualTo(emp.getEmail());
+		emp.setStatus(true);
+		emp.setUpdated(new Date().getTime() / 1000);
+		emp.setRole("employee");
+		int i = empMapper.updateByExampleSelective(emp, example);
+		assertEquals(true, i > 0);
+	}
+
+	@Test
+	@Ignore
+	public void testCreateSession() {
+		emp.setId(34);
+		emp.setFirstName("abc");
+		emp.setEmail("abc@newput.com");
+		session.setEmpId(emp.getId());
+		session.setEmpName(emp.getFirstName());
+		session.setToken(util.createSessionKey(getCurrentTime(), emp.getEmail()));
+		session.setCreated(getCurrentTime());
+		session.setExpiresWhen(getCurrentTime() + 3600);
+		int i = sessionMapper.insertSelective(session);
+		assertEquals(true, i > 0);
+
+	}
+
+	@Test
+	@Ignore
+	public void testSignOut() {
+		SessionExample sessionExample = new SessionExample();
+		sessionExample.createCriteria().andEmpIdEqualTo(34);
+		List<Session> sessionList = sessionMapper.selectByExample(sessionExample);
+		Session localSession = sessionList.get(0);
+		localSession.setExpiresWhen(getCurrentTime());
+		int i = sessionMapper.updateByExample(localSession, sessionExample);
+		assertEquals(true, i > 0);
+	}
+
+	@Test
+	@Ignore
+	public void testTimeSheetValue() {
+		timeSheet = reqParser.setTimeSheetValue("25-10-2015", "9:00", "19:00", "1", 34);
+		boolean status = timeSchedual.saveTimeSheet(timeSheet);
+		assertEquals(true, status);
+	}
+
+	@Test
+	@Ignore
+	public void testDateSheetValue() {
+		dateSheet.setCreated(getCurrentTime());
+		dateSheet.setEmpId(34);
+		dateSheet.setWorkDate(getCurrentTime());
+		dateSheet.setWorkDesc("Test case entry");
+
+		int j = dateSheetMapper.insertSelective(dateSheet);
+		assertEquals(true, j > 0);
+	}
+
+	@Test
+	@Ignore
+	public void testResetPassword() {
+		emp.setEmail("abc@newput.com");
+		EmployeeExample example = new EmployeeExample();
+		example.createCriteria().andEmailEqualTo(emp.getEmail());
+		List<Employee> empl = empMapper.selectByExample(example);
+		Employee emply = empl.get(0);
+		emply.setpToken("5869");
+		emply.setpExpireAt(getCurrentTime() + 30);
+		emply.setUpdated(getCurrentTime());
+		int i = empMapper.updateByExampleSelective(emply, example);
+		assertEquals(true, i > 0);
+	}
+
+	@Test
+	@Ignore
+	public void testPwdVerify() {
+		emp.setId(34);
+		emp.setpToken("5869");
+		emp.setPassword("abcd");
+		int i = 0;
+
+		EmployeeExample example = new EmployeeExample();
+		example.createCriteria().andIdEqualTo(emp.getId()).andPTokenEqualTo(emp.getpToken());
+		List<Employee> employeeList = empMapper.selectByExample(example);
+		System.out.println(employeeList.size());
+		if (employeeList.size() > 0) {
+			Employee employee = employeeList.get(0);
+			if (employee.getpExpireAt() >= getCurrentTime()) {
+				example.createCriteria().andIdEqualTo(emp.getId());
+				employee.setPassword(emp.getPassword());
+				employee.setUpdated(new Date().getTime() / 1000);
+				i = empMapper.updateByExampleSelective(employee, example);
+				assertEquals(true, i > 0);
+			} else {
+				assertEquals(true, i = 0);
+			}
+		} else {
+			assertEquals(true, i = 0);
+		}
 	}
 	
 	@Test
