f98a5a57d6a9ab7aa25783f617ea2adfc691106f
rahul <rahul@newput.com>
2015-11-16 11:29:52 +0530

    update test case

--- src/test/java/com/newput/testCase/ControllerTestCase.java
+++ src/test/java/com/newput/testCase/ControllerTestCase.java
@@ -4,6 +4,7 @@
 //import org.junit.Test;
 //import org.junit.runner.RunWith;
 //import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
 //import org.springframework.test.context.ContextConfiguration;
 //import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 //
@@ -26,7 +27,7 @@
 //import java.text.SimpleDateFormat;
 //import java.util.Date;
 //
-//
+//@Service
 //@RunWith(SpringJUnit4ClassRunner.class)
 //@ContextConfiguration(locations = "/applicationContext.xml")
 //public class ControllerTestCase {
